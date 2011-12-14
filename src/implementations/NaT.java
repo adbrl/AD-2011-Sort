069d22240fc22340a89149706231e1a3c1665acd
andy <andreaswimmer.aw@googlemail.com>
2011-12-14 19:10:26 +0100

    Added UNIX Time (start) as Date for NaT

--- src/implementations/NaT.java
+++ src/implementations/NaT.java
@@ -2,7 +2,11 @@ package implementations;
 
 import interfaces.Termin;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -30,8 +34,16 @@ public class NaT implements Termin {
 	 * @see interfaces.Termin#datum()
 	 */
 	@Override
-	public Date datum() {
-		return null;
+	public Date datum(){
+		//1. Januar 1970 00:00 Uhr UTC
+		Date result = null;
+		DateFormat df = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.UK);
+		try {
+			result = df.parse("Thu Jan 01 00:00:00 UTC 1970");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 	
 	/* (non-Javadoc)
